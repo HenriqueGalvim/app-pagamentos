560a3747d230a81a0f96632467341f499547ad2b
Henrique <henrixe80@gmail.com>
2025-05-21 19:16:24 -0400

    Adicionando o formulário de pesquisa de pessoas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LancamentosComponent } from './lancamentos/lancamentos.component';
 import { LayoutComponent } from './layout/layout.component';
+import { PessoasComponent } from './pessoas/pessoas.component';
 
 export const routes: Routes = [
   {
@@ -8,7 +9,8 @@ export const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: 'lancamentos', component: LancamentosComponent },
-      { path: '', redirectTo: 'lancamentos', pathMatch: 'full' }
-    ]
-  }
+      { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
+      { path: 'pessoas', component: PessoasComponent },
+    ],
+  },
 ];
