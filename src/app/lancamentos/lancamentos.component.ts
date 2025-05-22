e7f314cf8f986f104214c1c1c595fdc6b1120c3c
Henrique <henrixe80@gmail.com>
2025-05-21 20:34:20 -0400

    Criação do componente filho de tabela de lançamentos

--- src/app/lancamentos/lancamentos.component.ts
+++ src/app/lancamentos/lancamentos.component.ts
@@ -1,42 +1,30 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import { LancamentosTableComponent } from '../lancamentos-table/lancamentos-table.component';
 
 @Component({
   selector: 'app-lancamentos',
   standalone: true,
   imports: [
     FormsModule,
-    CommonModule,
-    MatToolbarModule,
-    MatPaginatorModule,
-    MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatTableModule,
-    MatTooltipModule,  RouterModule
+    RouterModule,
+    LancamentosTableComponent
   ],
   templateUrl: './lancamentos.component.html',
   styleUrl: './lancamentos.component.scss'
 })
-export class LancamentosComponent implements OnInit, AfterViewInit {
+export class LancamentosComponent {
   descricao = '';
   vencimentoInicio = '';
   vencimentoFim = '';
-
-  colunas: string[] = ['pessoa', 'descricao', 'vencimento', 'pagamento', 'valor', 'acoes'];
 
   lancamentos = [
     { tipo: 'RECEITA', pessoa: 'Henrique Medeiros', descricao: 'Bahamas', vencimento: '10/06/2017', pagamento: null, valor: 500.00 },
@@ -47,12 +35,4 @@ export class LancamentosComponent implements OnInit, AfterViewInit {
   ];
 
   dataSource = new MatTableDataSource(this.lancamentos);
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-  }
 }
