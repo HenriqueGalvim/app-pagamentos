f45f6030ae657eaa80f9394fe73cf0a14d834ef4
Henrique <henrixe80@gmail.com>
2025-06-18 14:51:52 -0400

    Atualizando o Componente PessoaCadastroComponent para Salvar Pessoas no Backend

--- src/app/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -4,12 +4,12 @@ import { RouterModule } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { NgxMaskDirective } from 'ngx-mask';
+import { Pessoa } from '../models/pessoa.model';
+import { PessoasService } from '../services/pessoas.service';
 
 @Component({
   selector: 'app-pessoa-cadastro',
   standalone: true,
-  templateUrl: './pessoa-cadastro.component.html',
-  styleUrl: './pessoa-cadastro.component.scss',
   imports: [
     FormsModule,
     ReactiveFormsModule,
@@ -17,27 +17,54 @@ import { NgxMaskDirective } from 'ngx-mask';
     MatInputModule,
     MatButtonModule,
     NgxMaskDirective
-  ]
+  ],
+  templateUrl: './pessoa-cadastro.component.html',
+  styleUrl: './pessoa-cadastro.component.scss'
 })
 export class PessoaCadastroComponent {
   form: FormGroup;
 
-constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private pessoasService: PessoasService
+  ) {
     this.form = this.fb.group({
       nome: ['', [Validators.required, Validators.minLength(5)]],
-      logradouro: ['', Validators.required],
-      numero: ['', Validators.required],
-      complemento: [''],
-      bairro: ['', Validators.required],
-      cep: ['', Validators.required],
-      cidade: ['', Validators.required],
-      estado: ['', Validators.required],
-     });
+      logradouro: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(255)]],
+      numero: ['', [Validators.required, Validators.maxLength(10)]],
+      complemento: ['', [Validators.maxLength(255)]],
+      bairro: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+      cep: ['', [Validators.required]],
+      cidade: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+      estado: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+    });
   }
 
   salvar() {
     if (this.form.valid) {
-      console.log('Dados salvos:', this.form.value);
+      const pessoa: Pessoa = {
+        nome: this.form.value.nome,
+        ativo: true,
+        endereco: {
+          logradouro: this.form.value.logradouro,
+          numero: this.form.value.numero,
+          complemento: this.form.value.complemento,
+          bairro: this.form.value.bairro,
+          cep: this.form.value.cep.trim(),
+          cidade: this.form.value.cidade,
+          estado: this.form.value.estado
+        }
+      };
+
+      this.pessoasService.criarPessoa(pessoa).subscribe({
+        next: (p) => {
+          console.log('Pessoa criada com sucesso:', p);
+          this.novo();
+        },
+        error: (e) => {
+          console.error('Erro ao criar pessoa:', e);
+        }
+      });
     }
   }
 
